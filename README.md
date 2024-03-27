b2412bd234d79ff11b14adee743cc138bce69cd3
Chersito <reyes.carlos0403@gmail.com>
2024-03-27 14:28:14 -0600

    Código Completo Explicado

--- README.md
+++ README.md
@@ -7,19 +7,82 @@
 ---
 
 ### Acerca del Proyecto
--
+- La condición de carrera ocurre cuando el resultado de una operación depende del tiempo relativo de ejecución de las operaciones concurrentes, lo que puede llevar a resultados inesperados y potencialmente incorrectos.
+- El interbloqueo se produce cuando dos o más procesos están esperando indefinidamente por recursos que están siendo utilizados por otros procesos, lo que resulta en un estancamiento del sistema.
+- Por otra parte, la lógica temporal es fundamental para especificar el comportamiento temporal de sistemas concurrentes y garantizar su corrección mediante técnicas de verificación formal.
+1. Desarrollar un programa que valide la eficiencia de los robots cosechadores en dos invernaderos, el primer invernadero está ubicado en la región de Chiapas (arreglo A) y el segundo ubicado en Cuernavaca (arreglo B).
+
+2.	El programa deberá presentar la eficiencia y efectividad de la cosecha automatizada a través de:
+    - Totalizar el número de frutas y/o verduras maduras que fueron cosechados por los diferentes robots industriales (estos robots son monitoreados por los hilos del programa).
+    - Presentar el porcentaje de eficiencia y error en la cosecha por producto y por invernadero.
+    - Presentar un mensaje indicando si se llegó o no al 100% de eficiencia.
+
 
 ### **Ejercicio:**
+A. Hilo del main
+1. El programa principal es el encargado de crear los arreglos A y B, ejecutar los hilos correspondientes asociados a los robots que realizan la cosecha (llenado de las cajas) en cada invernadero, ejecutar el hilo encargado de analizar (totalizar) la cosecha de ambos invernaderos y presentar las estadísticas y recomendaciones.
+Datos de entrada:
+Entero n: es el tamaño del arreglo y representa el número de productos (fruta o verdura) distintos a cosechar en ambos invernaderos.
+Ejemplo:
+    - En el renglón 0 se almacenará manzanas cosechadas en estado óptimo
+    - En el renglón 1 se almacenará pepinillos cosechados en estado óptimo
+    - …
+    - En el renglón n-1 se almacenará fresas cosechadas en estado óptimo
+
+B. Hilo Cosechador
+
+Existe un único robot (hilo) cosechador por invernadero, encargado de realizar toda la cosecha automatizada (número de renglones del arreglo).
+1. Realizar la cosecha y llenar el arreglo con números aleatorios que representan el número de fruta o verdura cosechada exitosamente, es decir, madura y en perfectas condiciones. Este número varía entre 90 y 100 por caja y representa el total de productos cosechados con éxito por día.
+2. Cada casilla del arreglo representa una caja de fruta o verdura diferente, por ejemplo, la casilla 0 puede representar una caja de tomates, la casilla 1 una caja de pepinos, la casilla 2 una caja de fresa y así sucesivamente.
+```
+main: ¿Cuántos productos diferentes se cosecharán?: 3
+Inicia cosecha Robot_Chiapas
+Inicia cosecha Robot_Cuernavaca
+---------------------------------------------------------------------
+        [Robot_Cuernavaca]:        Cosechando ... Manzana
+        [Robot_Cuernavaca]:        Cosechando ... Plátano
+        [Robot_Chiapas]:        Cosechando ... Manzana
+        [Robot_Cuernavaca]:        Cosechando ... Fresa
+        [Robot_Chiapas]:        Cosechando ... Plátano
+        [Robot_Chiapas]:        Cosechando ... Fresa
+
+Robot_Chiapas terminó la cosecha del día
+Robot_Cuernavaca terminó la cosecha del día
+```
+_Nota: El output puede variar dependiendo de la condición de competencia entre hilos y procesos_
+
+C. Hilo Analizador del producto
+1. El hilo analizador del producto es el encargado de realizar el proceso de totalizar los diferentes productos cosechados con las características optimas en los invernaderos y generar las estadísticas de la cosecha automatizada, donde 200 productos cosechados con éxito representarán el 100%
+```
+Inicia análisis de cosecha Analizador
+---------------------------------------------------------------------
+        [AnalizadorProducto]: Analizando ... Manzana
+        [AnalizadorProducto]: Analizando ... Plátano
+        [AnalizadorProducto]: Analizando ... Fresa
+
+                ESTADÍSTICAS DE LOS INVERNADEROS
+---------------------------------------------------------------------
+Producto   Chiapas   Cuernavaca     Total   %Exito   %Error
+---------------------------------------------------------------------
+Manzana   99         99                198         99.0   1.0
+Analizador ---> Aún se debe calibrar los sistemas de IA para obtener el 100%
+Plátano   95         90                185         92.5   7.5
+Analizador ---> Aún se debe calibrar los sistemas de IA para obtener el 100%
+Fresa     94         100                       194         97.0   3.0
+Analizador ---> Aún se debe calibrar los sistemas de IA para obtener el 100%
+---------------------------------------------------------------------
+```
+
 
 _Nota: El output puede variar dependiendo de la condición de competencia entre hilos y procesos_
 
 
 ### Para abrir el código :
 Opción 1 :
-1. Abrir la carpeta de [src]
+1. Abrir la carpeta de [src](https://github.com/Chersito/Act3_CosechaAutomatica1/tree/main/src)
 
-2. Código en  [CosechaAutomática1.java]
+2. Código en  [CosechaAutomática1.java](https://github.com/Chersito/Act3_CosechaAutomatica1/blob/main/src/CosechaAutomatica1.java)
 
 Opción 2 :
 
-Utiliza `git clone ` para obtener el repositorio por medio de tu terminal
+Utiliza `git clone https://github.com/Chersito/Act3_CosechaAutomatica1.git` para obtener el repositorio por medio de tu terminal
--- src/CosechaAutomatica1.java
+++ src/CosechaAutomatica1.java
@@ -1,19 +1,26 @@
+// Libreria Random genera números aleatorios
 import java.util.Random;
+// Libreria Scanner lee datos de entrada en consola
 import java.util.Scanner;
 
+// Clase Cosechador que extiende de Thread para manejar hilos
 class Cosechador extends Thread {
+    // Vectores que almacenan los nombres de frutas, verduras y su cantidad
     private final int[] invernadero;
     private final String[] fruits;
     private final String[] vegetables;
 
+    // Constructor con invernadero tamaño n
     public Cosechador(int[] invernadero, String[] fruits, String[] vegetables, int n) {
         this.invernadero = new int[n];
         this.fruits = fruits;
         this.vegetables = vegetables;
     }
 
+    // Sobreescribir el método run()
     @Override
     public void run() {
+        // Itera cada producto para que aparezca en consola
         Random random = new Random();
         for (int i = 0; i < invernadero.length; i++) {
             String product;
@@ -27,12 +34,15 @@ class Cosechador extends Thread {
     }
 }
 
+// Clase Analizador que extiende de Thread para manejar hilos
 class Analizador extends Thread {
+    // invernadores contienen los datos, products tienen el nombre del dato
     private final int[] invernaderoA;
     private final int[] invernaderoB;
     private final String[] products;
     private final int n;
 
+    //Constructor
     public Analizador(int[] invernaderoA, int[] invernaderoB, String[] products, int n) {
         this.invernaderoA = invernaderoA;
         this.invernaderoB = invernaderoB;
@@ -40,10 +50,12 @@ class Analizador extends Thread {
         this.n = n;
     }
 
+    // Sobreescribir el método run()
     @Override
     public void run() {
         System.out.println("\nInicia análisis de cosecha Analizador");
         System.out.println("---------------------------------------------------------------------");
+        // Itera cada producto para que aparezca en consola
         for (int i = 0; i < products.length; i++) {
             if (i < n) {
                 System.out.println("\t[AnalizadorProducto]: Analizando ... " + products[i]);
@@ -57,41 +69,57 @@ class Analizador extends Thread {
         Random random = new Random();
         for (int i = 0; i < n; i++) {
             String product = products[i];
-            int totalA = random.nextInt(11) + 90;
-            int totalB = random.nextInt(11) + 90;
+            int totalA = random.nextInt(11) + 90; //Número random del 90 al 100
+            int totalB = random.nextInt(11) + 90; //Número random del 90 al 100
             int total = totalA + totalB;
             double exito = (double) total / 2;
             double error = 100 - exito;
 
             System.out.printf("%s\t  %d\t     %d\t\t       %d\t   %.1f\t  %.1f%n", product, totalA, totalB, total, exito, error);
+
+            // Verificar si el éxito es igual a 100 o no
+            if (exito == 100) {
+                System.out.println("¡Analizador ---> La cosecha de " + product + " ha alcanzado el 100% de éxito!");
+            } else {
+                System.out.println("Analizador ---> Aún se debe calibrar los sistemas de IA para obtener el 100%");
+            }
         }
         System.out.println("---------------------------------------------------------------------");
     }
 }
 
 
-
+// Clase Pública
 public class CosechaAutomatica1 {
+    // Se crean cadenas con los nombres de frutas y verduras respectivamente
     private static final String[] FRUITS = {"Manzana", "Plátano", "Fresa", "Naranja", "Pera", "Uva", "Piña", "Mango", "Sandía", "Cereza"};
     private static final String[] VEGETABLES = {"Pepinillo", "Lechuga", "Tomate", "Zanahoria", "Cebolla", "Pimiento", "Brócoli", "Espinaca", "Calabacín", "Apio"};
+    // Representa cada invernadero, utilizando los datos de frutas y verduras después
     private static int[] invernaderoA; // 10 frutas + 10 verduras
     private static int[] invernaderoB; // 10 frutas + 10 verduras
 
+    // MAIN
     public static void main(String[] args) throws InterruptedException {
+        // Se crea objeto Scanner para leer la entrada del usuario
         Scanner scanner = new Scanner(System.in);
 
+        // Se pide al usuario introducir el dato
         System.out.print("\nmain: ¿Cuántos productos diferentes se cosecharán?: ");
         int n = scanner.nextInt();
 
+        // Se inicializan los Vectores con el tamaño n ya predefinido
         invernaderoA = new int[n];
         invernaderoB = new int[n];
 
+        // Se crean objetos de la clase Cosechador que contienen hilos
         Cosechador cosechadorA = new Cosechador(invernaderoA, FRUITS, VEGETABLES, n);
         Cosechador cosechadorB = new Cosechador(invernaderoB, FRUITS, VEGETABLES, n);
         
+        // Se asigna nombre a los hilos
         cosechadorA.setName("Robot_Chiapas");
         cosechadorB.setName("Robot_Cuernavaca");
         
+        // Se inicializan los hilos
         cosechadorA.start();
         cosechadorB.start();
 
@@ -99,15 +127,18 @@ public class CosechaAutomatica1 {
         System.out.println("Inicia cosecha Robot_Cuernavaca");
         System.out.println("---------------------------------------------------------------------");
 
+        // Se espera hasta que terminen los hilos su ejecución
         cosechadorA.join();
         cosechadorB.join();
 
         System.out.println("\nRobot_Chiapas terminó la cosecha del día");
         System.out.println("Robot_Cuernavaca terminó la cosecha del día");
 
+        // Se crea objeto de clase Analizador y se inicializa
         Analizador analizadorProducto = new Analizador(invernaderoA, invernaderoB, FRUITS, n);
         analizadorProducto.start();
 
+        // Se detienen scanner
         scanner.close();
     }
 }
